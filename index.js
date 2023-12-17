7b400df16c4520cd877d19d318e9be48a1d1b8c7
OGrabill <143433050+OGrabill@users.noreply.github.com>
2023-12-16 18:13:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
 // Add a rectangle at (10, 10) with size 100x100 pixels
 ctx.fillRect(10, 10, 100, 100);
+while (true) {
+
+    rect(mouseX,mouseY,40,40);
+
+}
