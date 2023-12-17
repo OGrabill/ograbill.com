afa979ac88f192cf49f6ed0b08c467c89af6a4ce
OGrabill <143433050+OGrabill@users.noreply.github.com>
2023-12-16 18:16:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
 // Add a rectangle at (10, 10) with size 100x100 pixels
 
-while (true) {
 
-    ctx.rect(mouseX,mouseY,40,40);
-    ctx.fillRect(10, 10, 100, 100);
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "green";
+// Add a rectangle at (10, 10) with size 100x100 pixels
+ctx.fillRect(10, 10, 100, 100);
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
 }
+canvas.addEventListener('mousemove', function(evt) {
+  var mousePos = getMousePos(canvas, evt);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(mousePos.x, mousePos.y, 10, 10);
+}, false);
+```
