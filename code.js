251c37798f3c17777f1f85f5dfff4300c6eec2ef
OGrabill <143433050+OGrabill@users.noreply.github.com>
2024-03-22 12:43:38 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,168 +1,121 @@
+let circleAngle = 0;
+const w = window.innerWidth;
+const h = window.innerHeight;
+const knifeAngle = 360 / (200 * Math.PI / 20) - 4;
+const cooldown = 5;
+let knifeDistance = 120;
+let shotKnives = [];
+let connectedKnives = [];
+let knifeSpeed = 10;
+let dead = 0;
+let pressed = false;
+let lastShot = Number.NEGATIVE_INFINITY;
 
-var programCode = function (p) {
-    p.size(
-        window.innerWidth,
-        window.innerHeight,
-        window.focus()
-    );
-    p.frameRate(60);
-    let circleAngle = 0;
-    
-    // useful constants
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    // how close knives can be before a game over
-    const knifeAngle = 360 / (200 * Math.PI / 20) - 4;
-    // time between shots, in milliseconds
-    const cooldown = 5;
+function setup() {
+  createCanvas(w, h);
+  frameRate(60);
+}
 
-    var knifeDistance = 120;
-    
-    // y-positions of all knives moving towards face (one not thrown is fake)
-    var shotKnives = [];
-    // angles relative to rotating face of all knives stuck on face
-    var connectedKnives = [];
+function drawKnife(x, y) {
+  translate(x, y);
+  fill(0);
+  rect(x - 10, y - 10, 20, 20);
+  translate(-x, -y);
+}
 
-    var knifeSpeed = 10;
+function keyPressed() {
+  if (pressed) return;
+  pressed = true;
+  const time = millis();
+  if (keyCode === 32 && time - lastShot > cooldown && dead === 0) {
+    shotKnives.push(knifeDistance);
+    lastShot = time;
+  }
+  if (keyCode === 32 && dead === 1) {
+    connectedKnives.length = 0;
+    dead = 0;
+  }
+}
 
-    var knifeCuttof = 100;
+function keyReleased() {
+  pressed = false;
+}
 
-    var dead = 0;
+function mouseClicked() {
+  console.log("clicked");
+  const time = millis();
+  if (time - lastShot > cooldown && dead === 0) {
+    shotKnives.push(knifeDistance);
+    lastShot = time;
+  }
+  if (dead === 1) {
+    connectedKnives.length = 0;
+    dead = 0;
+  }
+}
 
-    var drawKnife = function (x, y) {
-        p.translate(x, y);
-        p.fill(0, 0, 0);
-        p.rect(x - 10, y - 10, 20, 20);
-        p.translate(-x, -y);
-    };
+function draw() {
+  background(255);
+  translate(w / 2, h / 2);
+  rotate(radians(circleAngle));
+  fill(255, 255, 0);
+  ellipse(0, 0, 200, 200);
+  noFill();
+  stroke(0);
+  strokeWeight(2);
+  arc(0, 0, 150, 100, 0, PI);
+  fill(0);
+  ellipse(50, 0, 10, 10);
+  ellipse(-47, 0, 10, 10);
 
-    // Add new knife when key pressed
-    let pressed = false;
-    let lastShot = Number.NEGATIVE_INFINITY;
-    p.keyPressed = function () {
-        if (pressed) return;
-        pressed = true;
-        const time = p.millis();
-        if (p.key.code === 32 && time - lastShot > cooldown && dead === 0) {
-            shotKnives.push(knifeDistance);
-            lastShot = time;
+  for (const angle of connectedKnives) {
+    rotate(radians(angle));
+    drawKnife(54, 0);
+    rotate(radians(-angle));
+  }
+  console.log(connectedKnives);
+  rotate(radians(-circleAngle));
+
+  fill(0);
+  textSize(60);
+  text("Score: "+connectedKnives.length, -110, -200);
+
+  if (dead === 0) {
+    circleAngle += 3;
+    circleAngle %= 360;
+  }
+
+  let newShotKnives = [];
+  for (let y of shotKnives) {
+    drawKnife(0, y);
+    y -= knifeSpeed;
+    if (y < 54) {
+      console.log("Collision");
+      const angle = (90 - circleAngle + 360) % 360;
+      for (const existingAngle of connectedKnives) {
+        if (
+          Math.abs(angle - existingAngle) < knifeAngle ||
+          Math.abs(angle - existingAngle) > 360 - knifeAngle
+        ) {
+          dead = 1;
         }
-        if (p.key.code === 32 && dead === 1) {
-            connectedKnives.length = 0;
-            dead = 0;
-        }
-    };
+      }
+      if (dead === 0) {
+        connectedKnives.push(angle);
+      }
+    } else {
+      newShotKnives.push(y);
+    }
+  }
 
-    p.keyReleased = function () {
-        pressed = false;
-    };
+  shotKnives = newShotKnives;
 
-    p.mouseClicked = function () {
-        console.log("clicked");
-        const time = p.millis();
-        if (time - lastShot > cooldown && dead === 0) {
-            shotKnives.push(knifeDistance);
-            lastShot = time;
-        }
-        if (dead === 1) {
-            connectedKnives.length = 0;
-            dead = 0;
-        }
-    };
-    
-    p.draw = function () {
-        // Color background
-        p.background(255, 255, 255);
+  if (dead === 0) {
+    drawKnife(0, knifeDistance);
+  }
 
-        p.translate(w / 2, h / 2);
-        p.rotate(p.radians(circleAngle));
-        p.fill(255, 255, 0);
-        p.ellipse(0, 0, 200, 200);
+  if (dead === 1) {
+    text("womp womp", -150, -160);
+  }
+}
 
-        p.noFill();
-        p.stroke(0, 0, 0);
-        p.strokeWeight(2);
-        p.arc(0, 0, 150, 100, 0, Math.PI);
-
-        p.fill(0, 0, 0);
-        p.ellipse(50, 0, 10, 10);
-        p.ellipse(-47, 0, 10, 10);
-
-        for (const angle of connectedKnives) {
-            p.rotate(p.radians(angle));
-            drawKnife(54, 0);
-            p.rotate(p.radians(-angle));
-        }
-        console.log(connectedKnives);
-
-        p.rotate(p.radians(-circleAngle));
-
-        p.fill(0, 0, 0);
-
-        p.textSize(60);
-        p.text("Score: "+connectedKnives.length,-110,-200);
-        //println(rot+" , "+w+" , "+w)
-        
-        // Make circle rotate if not dead
-        if (dead === 0) {
-        circleAngle += 3;
-        circleAngle %= 360;
-        }
-        
-        // Render each knife
-        const newShotKnives = [];
-        for (let y of shotKnives) {
-            drawKnife(0, y);
-            // Subtract one from each knife's position
-            y -= knifeSpeed;
-            if (y < 54) {
-                // When knife has hit
-                console.log("Collision");
-                const angle = (90 - circleAngle + 360) % 360;
-                for (const existingAngle of connectedKnives) {
-                    if (
-                        Math.abs(angle - existingAngle) < knifeAngle
-                        || Math.abs(angle - existingAngle) > 360 - knifeAngle
-                    ) {
-                        //alert(`You lose!\nScore: ${connectedKnives.length}`);
-                        dead = 1;
-                    }
-                }
-                if (dead === 0) {
-                connectedKnives.push(angle);
-                }
-            } else {
-                // delete all knives that have gone too far from list
-                newShotKnives.push(y);
-            }
-        }
-        
-        shotKnives = newShotKnives;
-
-        // Draw fake knife if not dead
-        if (dead === 0) {
-        drawKnife(0, knifeDistance);
-        }
-
-        if (dead === 1) {
-            
-            p.text("womp womp",-150,-160);
-        }
-
-        
-        
-        p.mouseClicked = function () {
-            if (dead === 1) {
-                connectedKnives.length = 0;
-                dead = 0;
-            }
-        ;
-    };
-
-    };
-
-    // Code end:
-};
-
-const canvas = document.getElementById("mycanvas");
-const processingInstance = new Processing(canvas, programCode);
